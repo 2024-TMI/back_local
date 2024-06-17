e9db30bf103a7dbab37c411d7a9a92b312ec38a1
steamedEggMaster <cksdud5014@naver.com>
2024-06-17 17:28:50 +0900

    [develop] return 설정

--- src/main/java/com/example/back_local/controller/GroupController.java
+++ src/main/java/com/example/back_local/controller/GroupController.java
@@ -2,6 +2,7 @@ package com.example.back_local.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,10 +15,10 @@ public class GroupController {
     private Logger LOGGER = LoggerFactory.getLogger(GroupController.class);
 
     @PostMapping("/create")
-    public void createGroup(@RequestParam("name") String groupName, @RequestParam("type") String type){
+    public ResponseEntity<String> createGroup(@RequestParam("name") String groupName, @RequestParam("type") String type){
         LOGGER.info("---------createGroup----------");
         LOGGER.info("groupName : {}, type : {}", groupName, type);
 
-
+        return ResponseEntity.ok("Hello");
     }
 }
